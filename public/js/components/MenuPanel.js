672211365958cbd0009b41316f074ecd4165265f
drumforhim <drumforhim@gmail.com>
2014-05-03 01:18:00 +0300

    first

--- public/js/components/MenuPanel.js
+++ public/js/components/MenuPanel.js
@@ -10,27 +10,14 @@ $.extend({
 
         this.list = $("<ul />");
         
-
-        this.createTree = function() {
-            for (var i = 0; i < this.options.nodes.length; i++) {
-                var node = this.options.nodes[i];
-                this.list.append(this.createNode(node));
-            }
+        this.initComponent = function(){
             this.list.menu();
-            this.createTitle();
+            this.options.renderTo.append(this.list);
         };
-
-        this.createList = function(nodes, li) {
-            var ls = $("<ul />");
-            for (var i = 0; i < nodes.length; i++) {
-                ls.append(this.createNode(nodes[i]));
-            }
-            return ls;
-        };
-
-        this.createNode = function(node) {
+        
+        this.createNode = function(node){
             var li = $("<li />"),
-                    a = $("<a />");
+                a = $("<a />");
 
             a
                     .attr("href", "#")
@@ -38,36 +25,75 @@ $.extend({
                     .html(node.label);
 
             li.append(a);
-            if (node.children) {
-                var ls = this.createList(node.children, li);
-                li.append(ls);
+            if(node.children){
+                for(var i=0;i<node.children.length;i++){
+                    var ls = $("<ul />");
+                    ls.append(node.children[i]);
+                }
+                li.append(ls);                
             }
             
             return li;
         };
+        
 
-        this.createTitle = function() {
-            var title = $("<li />"),
-                    a = $("<a />");
-            a
-                    .attr("href", "#")
-                    .html(this.options.title);
-            title
-                    .addClass("ui-state-disabled")
-                    .append(a);
-
-            this.list.prepend(title);
-        };
-
-
-        this.initComponent = function() {
-            this.createTree(this.options.nodes);     
-
-            this.options.renderTo.append(this.list);
-        };
-
-
-        this.initComponent();
+//        this.createTree = function() {
+//            for (var i = 0; i < this.options.nodes.length; i++) {
+//                var node = this.options.nodes[i];
+//                this.list.append(this.createNode(node));
+//            }
+//            this.list.menu();
+//            this.createTitle();
+//        };
+//
+//        this.createList = function(nodes, li) {
+//            var ls = $("<ul />");
+//            for (var i = 0; i < nodes.length; i++) {
+//                ls.append(this.createNode(nodes[i]));
+//            }
+//            return ls;
+//        };
+//
+//        this.createNode = function(node) {
+//            var li = $("<li />"),
+//                    a = $("<a />");
+//
+//            a
+//                    .attr("href", "#")
+//                    .on("click", node.click)
+//                    .html(node.label);
+//
+//            li.append(a);
+//            if (node.children) {
+//                var ls = this.createList(node.children, li);
+//                li.append(ls);
+//            }
+//            
+//            return li;
+//        };
+//
+//        this.createTitle = function() {
+//            var title = $("<li />"),
+//                    a = $("<a />");
+//            a
+//                    .attr("href", "#")
+//                    .html(this.options.title);
+//            title
+//                    .addClass("ui-state-disabled")
+//                    .append(a);
+//
+//            this.list.prepend(title);
+//        };
+//
+//
+//        this.initComponent = function() {
+//            this.createTree(this.options.nodes);     
+//
+//            this.options.renderTo.append(this.list);
+//        };
+//
+//
+//        this.initComponent();
 
     }
 });
--- public/js/menus/MainMenu.js
+++ public/js/menus/MainMenu.js
@@ -1,37 +1,46 @@
 $.extend({
     MainMenu:function(westPanel){
         
-        
-        
-        var usersNode = {
-            label:"Users",
-            id:"users",
-            click:function(){
-                alert("Users");
-            }
-        };
-        
-        var customersNode = {
-            label: "Customers",
-            id:"customers",
-            click:function(){
-                alert("Customers");
-            }
-        };
-        
-        var personsNode = {
-            label:"Persons",
-            id:"persons",
-            children:[usersNode,customersNode]
-        };
-        
-        var rootNodes = [personsNode];
-        
         var mainMenu = new $.MenuPanel({
-            renderTo: westPanel,
-            title: Localizer.MAIN_MENU,
-            nodes:rootNodes
+           renderTo:westPanel,
+           title: Localizer.MAIN_MENU
         });
+        
+        var personsNode = mainMenu.createNode({
+            label:"Persons",
+            id:"persons"
+            
+        });
+        
+//        var usersNode = {
+//            label:"Users",
+//            id:"users",
+//            click:function(){
+//                alert("Users");
+//            }
+//        };
+//        
+//        var customersNode = {
+//            label: "Customers",
+//            id:"customers",
+//            click:function(){
+//                alert("Customers");
+//            }
+//        };
+//        
+//        var personsNode = {
+//            label:"Persons",
+//            id:"persons",
+//            children:[usersNode,customersNode]
+//        };
+//        
+//        var rootNodes = [personsNode];
+//        
+//        var mainMenu = new $.MenuPanel({
+//            renderTo: westPanel,
+//            title: Localizer.MAIN_MENU,
+//            nodes:rootNodes
+//        });
         
         
         
